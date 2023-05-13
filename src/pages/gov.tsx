0356e9fc46ff65a88347655bae82aca0bdbef276
c0rdeiro <franciscordeiro99@gmail.com>
2023-05-13 20:01:13 +0100

    gov screen by role

--- src/pages/gov.tsx
+++ src/pages/gov.tsx
@@ -1,3 +1,4 @@
+import { useAccount } from 'wagmi'
 import Bailiff from '~/components/Gov/Bailiff'
 import DefendantClaim from '~/components/Gov/Defendant/DefendantClaim'
 import DefendantVeredict from '~/components/Gov/Defendant/DefendantVeredict'
@@ -5,16 +6,33 @@ import Defense from '~/components/Gov/Defense'
 import Judge from '~/components/Gov/Judge/Judge'
 import Jury from '~/components/Gov/Jury'
 import Prosecution from '~/components/Gov/Prosecution'
+import roles from 'roles.json'
 
 export default function GovPage() {
+  const { address } = useAccount()
   const usersLayout = () => {
-    return <Jury juryNumber="1" />
-    return <DefendantClaim />
-    return <Judge />
-    return <Defense />
-    return <DefendantVeredict />
-    return <Bailiff />
-    return <Prosecution />
+    if (!address) return <></>
+    const roleToBe = roles.find((x) => x.Address === address)?.Role
+
+    switch (roleToBe) {
+      case 'Defandant':
+        return <DefendantClaim />
+      case 'Defense':
+        return <Defense />
+      case 'Prosecutor':
+        return <Prosecution />
+      case 'Jury1':
+        return <Jury juryNumber="1" />
+      case 'Jury2':
+        return <Jury juryNumber="2" />
+      case 'Jury3':
+        return <Jury juryNumber="3" />
+      case 'Judge':
+        return <Judge />
+      case 'Bailiff':
+        return <Bailiff />
+    }
+    // return <DefendantVeredict />
   }
 
   return (
