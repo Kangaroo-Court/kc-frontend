a5175df25c495c54a5e046a6a7e2af1e03ea3c77
c0rdeiro <franciscordeiro99@gmail.com>
2023-05-14 04:08:03 +0100

    cleanup

--- src/components/Gov/Judge/VeredictsModal.tsx
+++ src/components/Gov/Judge/VeredictsModal.tsx
@@ -13,35 +13,12 @@ const VeredictsModal: React.FC<VeredictsModalProps> = ({
   isOpen,
   setIsOpen,
 }) => {
-  const veredicts: { hash: string; jury: string; veredict: boolean }[] = [
-    {
-      hash: '0xa123ias1231j123123',
-      jury: 'Jury1',
-      veredict: false,
-    },
-    {
-      hash: '0xa123ias1231j123123',
-      jury: 'Jury2',
-      veredict: true,
-    },
-    {
-      hash: '0xa123ias1231j123123',
-      jury: 'Jury3',
-      veredict: false,
-    },
-  ]
-
-  // const easApolloClient = new ApolloClient({
-  //   uri: 'https://optimism-goerli.easscan.org/graphql',
-  //   cache: new InMemoryCache(),
-  // })
   //eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { data } = useQuery(GET_EAS_ATTESTATIONS, {
     variables: {
       schemaId:
         '0x56c13171ec212b1bd36ca786b7ed53678a03136863c85063ec0fa23f15e8fcee',
     },
-    // client: easApolloClient,
   })
 
   const parsedData = data?.attestations.map((x: any) =>
