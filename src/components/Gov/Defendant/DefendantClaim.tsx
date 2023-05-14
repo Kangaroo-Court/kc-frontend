aa994cb5aa5ae1aedb1ebfbff827121a35389523
c0rdeiro <franciscordeiro99@gmail.com>
2023-05-14 09:02:06 +0100

    end

--- src/components/Gov/Defendant/DefendantClaim.tsx
+++ src/components/Gov/Defendant/DefendantClaim.tsx
@@ -28,7 +28,7 @@ const DefendantClaim: React.FC = () => {
         imageSrc="/defendant.png"
         imageAlt="defendant"
         actionNode={
-          <div className="flex flex-col items-center gap-32">
+          <div className="flex flex-col items-center gap-20">
             <h1 className="text-6xl font-bold text-primary-600 underline">Defendant</h1>
             <div className="flex flex-col gap-10">
               <h3 className="text-3xl font-medium text-primary-600">Is ApeCoin dead by XXXXX?</h3>
